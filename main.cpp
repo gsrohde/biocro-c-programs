2d04d6f19e7a6b551853a9c9fa1dd2d59d2f3485
Scott Rohde <srohde@illinois.edu>
2022-03-22 11:49:36 -0500

    Makefile: Added BioCro.so and exampleLibrary.so as dependencies of the two executables.
    main.cpp: Test out like-named modules in different libraries.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
-#include "../example_biocro_module_library/src/framework/module_creator.h"
-#include "../biocro-dev-pristine/src/module_library/module_library.h"
+#include "../example_biocro_module_library/src/framework/module_factory.h"
+#include "../biocro-dev-pristine/src/framework/module_factory.h"
 
-#include "../biocro-dev-pristine/src/module_library/module_graph_test.hpp"
+//#include "../biocro-dev-pristine/src/module_library/module_graph_test.hpp"
 
 using namespace std;
-using BioCro::module_factory;
+
 
 void module_info(module_creator* w, bool verbose);
 
 void print_mods(string heading, string_vector mods);
 
 int main() {
-    string name { "Module_4" };
+    string name { "Module_3" };
 
+    string_vector mods1 = BioCro::module_factory::get_all_modules();
+    print_mods("First 8 modules in BioCro library", mods1);
 
-    ////////////////////////////////////////////////////////////////////////////////
-    // Testing out module_creator_impl
-    ////////////////////////////////////////////////////////////////////////////////
-    
-    // Get the module_creator pointer using module_creator_impl
-    module_creator* mc1 = new module_creator_impl<Module_3>;
+    printf("------------------------------------------\n\n");
+
+    // Get the module_creator pointer using retrieve
+    module_creator* mc1 = exampleLibrary::module_factory::retrieve(name);
     printf("Module_3 mc1 pointer is %p\n", mc1);
     printf("Printing module info:\n\n");
     module_info(mc1, true);
 
     printf("------------------------------------------\n\n");
 
-    ////////////////////////////////////////////////////////////////////////////////
-    // Testing get_all_modules
-    ////////////////////////////////////////////////////////////////////////////////
-
-    string_vector mods1 = module_factory::get_all_modules();
-    print_mods("First 8 modules in BioCro library", mods1);
+    string_vector mods2 = exampleLibrary::module_factory::get_all_modules();
+    print_mods("First 8 modules in exampleLibrary library", mods2);
 
     printf("------------------------------------------\n\n");
 
@@ -43,6 +39,7 @@ int main() {
     printf("Module_3 mc2 pointer is %p\n", mc2);
     printf("Printing module info:\n\n");
     module_info(mc2, true);
+
 }
 
 
